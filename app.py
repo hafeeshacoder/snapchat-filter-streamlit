40cc39c9ebd467434c3f959ac27bb59c29201382
hafeeshacoder <fathimahafeesha632@gmail.com>
2026-03-13 22:10:01 +0530

    Refactor mind map generation logic and visuals

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 import math
+import re
 from collections import Counter
 
 st.title("AI Mind Map Generator")
@@ -10,32 +11,52 @@ text = st.text_area("Enter your content")
 
 if st.button("Generate Mind Map"):
 
-    words = text.lower().split()
+    words = re.findall(r'\b[a-zA-Z]+\b', text.lower())
 
-    stopwords = ["is","the","a","an","and","of","to","in","for","on","with","that","this"]
+    stopwords = {
+        "the","is","a","an","and","of","to","in","for","on","with",
+        "that","this","it","as","are","was","be","by","from"
+    }
 
-    words = [w for w in words if w not in stopwords]
+    words = [w for w in words if w not in stopwords and len(w) > 3]
 
     freq = Counter(words)
 
-    keywords = [w for w,c in freq.most_common(6)]
+    keywords = [w for w,_ in freq.most_common(6)]
+
+    topic = keywords[0].capitalize()
 
     img = np.ones((800,1000,3),dtype=np.uint8)*255
 
     center = (500,400)
 
-    # center node
-    cv2.circle(img,center,80,(0,150,255),-1)
-    cv2.putText(img,"MAIN TOPIC",(430,405),
-                cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+    # Draw center topic
+    cv2.circle(img,center,90,(0,140,255),-1)
 
-    radius = 280
+    text_size = cv2.getTextSize(topic,
+                                cv2.FONT_HERSHEY_SIMPLEX,
+                                0.9,2)[0]
+
+    cv2.putText(img,
+                topic,
+                (center[0]-text_size[0]//2,
+                 center[1]+10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,(255,255,255),2)
+
+    radius = 300
     angle_step = 360/len(keywords)
 
-    colors = [(255,0,0),(0,200,0),(0,0,255),
-              (255,100,0),(200,0,200),(0,200,200)]
+    colors = [
+        (255,0,0),
+        (0,200,0),
+        (0,0,255),
+        (255,100,0),
+        (200,0,200),
+        (0,200,200)
+    ]
 
-    for i,word in enumerate(keywords):
+    for i,word in enumerate(keywords[1:]):
 
         angle = math.radians(i*angle_step)
 
@@ -44,18 +65,20 @@ if st.button("Generate Mind Map"):
 
         color = colors[i]
 
-        # branch
         cv2.line(img,center,(x,y),color,3)
 
-        # node
-        cv2.ellipse(img,(x,y),(90,40),0,0,360,color,-1)
+        cv2.ellipse(img,(x,y),(100,45),0,0,360,color,-1)
 
-        cv2.putText(img,word,(x-40,y+5),
-                    cv2.FONT_HERSHEY_SIMPLEX,0.6,(255,255,255),2)
+        txt = word.capitalize()
+
+        size = cv2.getTextSize(txt,
+                               cv2.FONT_HERSHEY_SIMPLEX,
+                               0.7,2)[0]
+
+        cv2.putText(img,
+                    txt,
+                    (x-size[0]//2,y+5),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7,(255,255,255),2)
 
     st.image(img,channels="BGR")
-
-    cv2.imwrite("mindmap.png",img)
-
-    with open("mindmap.png","rb") as f:
-        st.download_button("Download Mind Map",f,"mindmap.png")
